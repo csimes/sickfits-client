d7ea9f516e1bcd36cb6dd24df08be4c54bba4683
csimes <cherron@developdbycherron.com>
2022-01-25 20:54:34 -0600

    Added global styling to Page component

--- components/Header.js
+++ components/Header.js
@@ -20,7 +20,7 @@ const Logo = styled.h1`
 const HeaderStyles = styled.header`
   .bar {
     border-bottom: 10px solid var(--black, black);
-    display: grid
+    display: grid;
     grid-template-columns: auto 1fr;
     justify-content: space-between;
     align-items: center;
